57acc1dd9c446d076532bd33ce698de363d4d469
Kate Chirkina <milightning.ua@gmail.com>
2019-02-21 11:06:35 +0200

    Initial Commit

---
+++ WhoseFlag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoseFlag
+//
+//  Created by Catherine on 2/21/19.
+//  Copyright © 2019 Catherine Priazhnikova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
